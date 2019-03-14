fb47ae4cc8865528be0bbef9194e691e5bc5c352
Carol e Vinicius <carolfortunato@outlook.com>
2019-03-14 19:48:16 -0300

    all fields are mandatory
    Co-authored-by: Vinicius <viniksantana5@gmail.com>

--- spec/features/user_creates_account_spec.rb
+++ spec/features/user_creates_account_spec.rb
@@ -13,4 +13,16 @@ feature 'User creates account' do
    # expect(page).to have_css('h4', text: 'Bem-vindo, Carol!')
     expect(page).to have_content('Bem-vindo, Carol!')
   end
+
+  scenario 'and the name is mandatory' do
+    visit root_path
+    click_on 'Criar conta de usuário'
+    fill_in 'Nome', with: ''
+    fill_in 'Email', with: 'carol@gmail.com'
+    fill_in 'Senha', with: '12345678'
+    fill_in 'Repita sua senha', with: '12345678'
+    click_on 'Enviar'
+
+    expect(page).to have_content("Name can't be blank")
+  end
 end
